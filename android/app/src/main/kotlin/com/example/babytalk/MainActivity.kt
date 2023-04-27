ca6ec9334971dd7e4bd8fad43c1e46f8639b2cab
ouiam08 <ouiam.khoudraji@gmail.com>
2023-04-27 16:20:03 +0100

    new commit

---
+++ android/app/src/main/kotlin/com/example/babytalk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.babytalk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() => runApp(new MyApp());
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+
+    );
+  }
+}
